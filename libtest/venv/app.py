e8b5984a292f816c0fc1497144c82ff63118354c
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2020-06-06 19:15:39 +0400

    New service app

--- libtest/venv/app.py
+++ libtest/venv/app.py
@@ -1,10 +1,26 @@
 import pandas as pd
 import torch
+import sklearn
+import numpy
+import re
+import os
 from flask import Flask, jsonify, request
 import pickle
 
+# def tokenize_text_simple_regex(txt, min_token_size=2):
+#     """ This func tokenize text with TOKEN_RE applied ealier """
+#     txt = txt.lower()
+#     all_tokens = TOKEN_RE.findall(txt)
+#     return [token for token in all_tokens if len(token) >= min_token_size]
+#
+# def tokenize_corpus(texts, tokenizer=tokenize_text_simple_regex, **tokenizer_kwargs):
+#     """
+#     This func tokenize corpus of docs
+#     """
+#     return [tokenizer(text, **tokenizer_kwargs) for text in texts]
+
 # load model
-model = torch.load(open('model_dj01.sav','rb'))
+model = pickle.load(open('model_dj01.sav','rb'))
 
 # app
 app = Flask(__name__)
